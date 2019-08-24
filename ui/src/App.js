60d63052c627d2cdaa0072c18fb8fb7bbbde63a6
angelachen <angelachen085@gmail.com>
2019-08-24 02:18:17 -0400

    react-ui - added chatbot

---
+++ ui/src/App.js
@@ -0,0 +1,73 @@
+import React from 'react';
+import { Chat } from '@progress/kendo-react-conversational-ui';
+import '@progress/kendo-theme-bootstrap/dist/all.css';
+
+class App extends React.Component {
+    constructor(props) {
+        super(props);
+        this.user = {
+            id: 1,
+            avatarUrl: "https://via.placeholder.com/24/008000/008000.png"
+        };
+        this.bot = { id: 0 };
+        this.state = {
+            messages: [
+                {
+                    author: this.bot,
+                    suggestedActions: [
+                        {
+                            type: 'reply',
+                            value: 'Oh, really?'
+                        }, {
+                            type: 'reply',
+                            value: 'Thanks, but this is boring.'
+                        }
+                    ],
+                    timestamp: new Date(),
+                    text: "Hello, this is a demo bot. I don't do much, but I can count symbols!"
+                }
+            ]
+        };
+    }
+
+    addNewMessage = (event) => {
+        let botResponce = Object.assign({}, event.message);
+        botResponce.text = this.countReplayLength(event.message.text);
+        botResponce.author = this.bot;
+        this.setState((prevState) => ({
+            messages: [
+                ...prevState.messages,
+                event.message
+            ]
+        }));
+        setTimeout(() => {
+            this.setState(prevState => ({
+                messages: [
+                    ...prevState.messages,
+                    botResponce
+                ]
+            }));
+        }, 1000);
+    };
+
+    countReplayLength = (question) => {
+        let length = question.length;
+        let answer = question + " contains exactly " + length + " symbols.";
+        return answer;
+    }
+
+    render() {
+        return (
+            <div>
+                <Chat user={this.user}
+                    messages={this.state.messages}
+                    onMessageSend={this.addNewMessage}
+                    placeholder={"Type a message..."}
+                    width={400}>
+                </Chat>
+            </div>
+        );
+    }
+}
+
+export default App;
